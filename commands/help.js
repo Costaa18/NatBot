ebc23ba73d0b388ff998848bb2524f457f91dcf5
afonsovelosof <afonsovelosof@gmail.com>
2021-03-06 19:53:21 +0000

    Initial commit

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+  const help = args.join(' ');
+  message.channel.send('Prefix: --');
+  message.channel.send('Prefix: --');
+};
